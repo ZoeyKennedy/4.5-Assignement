d9c59ea63713c644a19d247dc2304bf473b331e7
Zoey Kennedy <339523656@GoTVDSB.ca>
2024-03-04 12:21:54 -0500

    drawing fish

--- Program.cs
+++ Program.cs
@@ -8,7 +8,7 @@ namespace _4._5_Assignement
         {
             int number;
             Console.WriteLine("press ENTER to reveal a fish");
-            Thread.DrawFish1(500);
+            //Thread.DrawFish1(500);
              
         }
         public static void DrawFish1()
